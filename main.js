794e06f90672b0e6f31eb4b1cbc0a74ecd9dcd72
gainizhumabek <111574565+gainizhumabek@users.noreply.github.com>
2022-10-24 22:26:03 +0600

    Commit message

---
+++ main.js
@@ -0,0 +1,28 @@
+function print(x, y, z) {
+return  x * y *z;
+}
+let a = print(114, 68, 13);
+console.log(a);
+
+let w = -5;
+if (w>0) {
+console.log("positive")
+}
+else if (w<0) {
+console.log("negative")
+}
+else {
+console.log("zero")
+}
+
+let q = 68;
+let p = 64;
+if (q>p) {
+console.log(q)
+}
+else if (q<p) {
+console.log(p)
+}
+else {
+console.log("the numbers are equal")
+}
